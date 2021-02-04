2004d3638749e60ec3e8180c3fbc131e78da1bb9
dhyanmohan <70349001+dhyanmohan@users.noreply.github.com>
2021-02-04 07:33:17 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+todoButton.addEventListener("click" , addTodo);
+todoList.addEventListener("click", deleteCheck);
